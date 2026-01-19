19a5dcbd6d903ff953ef0a4638b317ffc6d19547
Andre Barros <andrebolerbarros@gmail.com>
2026-01-19 23:01:58 +0000

    First interaction with pre-processing notebooks

---
+++ workflow/99_scripts/TRANS_017_EnvSetup.sh
@@ -0,0 +1,6 @@
+# pick a location you own; default is usually fine if your conda is user-installed
+mamba create -n py_scrnaseq -c conda-forge python=3.10 scanpy scvi-tools jupyterlab ipykernel
+conda activate py_scrnaseq
+python -m ipykernel install --user --name scrnaseq --display-name "scRNASeq - Python"
+
+mamba install -c conda-forge leidenalg python-igraph louvain statsmodels harmonypy scikit-misc
